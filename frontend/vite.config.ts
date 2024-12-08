efeff5ac71c1a6d36dcf04934bd45ab2da0af358
ledzz <ledzz1994@gmail.com>
2024-12-08 23:46:17 +0200

    wip

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.VITE_BASE_URL || '/',
   plugins: [react(),basicSsl()],
   server: {
     host: '0.0.0.0'
