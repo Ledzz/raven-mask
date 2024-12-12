2422acdfeb7221ac66be50a8a2af4cfe4e7083e0
ledzz <ledzz1994@gmail.com>
2024-12-12 18:10:58 +0200

    wip

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
-import './App.css'
-import {useCallback, useEffect, useRef} from "react";
+import "./App.css";
+import { useCallback, useEffect, useRef } from "react";
+import { HexColorPicker } from "react-colorful";
 
 let characteristic: BluetoothRemoteGATTCharacteristic | undefined;
 const serviceUuid = "4fafc201-1fb5-459e-8fcc-c5c9c331914b";
@@ -7,152 +8,184 @@ const characteristicUuid = "beb5483e-36e1-4688-b7f5-ea07361b26a8";
 
 const log = console.log;
 
+const MODES = ["SIMPLE", "EYES", "RANDOM"];
+
+const ZONES = [
+  {
+    id: 0,
+    name: "висок верх",
+  },
+  {
+    id: 1,
+    name: "брови",
+  },
+  {
+    id: 2,
+    name: "лоб",
+  },
+  {
+    id: 3,
+    name: "левый глаз",
+  },
+  {
+    id: 4,
+    name: "подбородок",
+  },
+  {
+    id: 5,
+    name: "клюв",
+  },
+  {
+    id: 6,
+    name: "щека",
+  },
+  {
+    id: 7,
+    name: "правый глаз",
+  },
+  {
+    id: 8,
+    name: "висок",
+  },
+];
+
 function App() {
-    const autoConnect = useCallback(async () => {
+  const autoConnect = useCallback(async () => {
+    try {
+      log("Getting existing permitted Bluetooth devices...");
+      const devices = await navigator.bluetooth.getDevices();
+
+      log("> Got " + devices.length + " Bluetooth devices.");
+      // These devices may not be powered on or in range, so scan for
+      // advertisement packets from them before connecting.
+      for (const device of devices) {
+        connectToDevice(device);
+      }
+    } catch (error) {
+      log("Argh! " + error);
+    }
+  }, []);
+
+  const connectToDevice = useCallback(async (device: BluetoothDevice) => {
+    const abortController = new AbortController();
+
+    device.addEventListener(
+      "advertisementreceived",
+      async (event: BluetoothAdvertisingEvent) => {
+        log('> Received advertisement from "' + device.name + '"...');
+        // Stop watching advertisements to conserve battery life.
+        abortController.abort();
+        log('Connecting to GATT Server from "' + device.name + '"...');
         try {
-            log('Getting existing permitted Bluetooth devices...');
-            const devices = await navigator.bluetooth.getDevices();
+          const server = await device.gatt?.connect();
+          log('> Bluetooth device "' + device.name + " connected.");
 
-            log('> Got ' + devices.length + ' Bluetooth devices.');
-            // These devices may not be powered on or in range, so scan for
-            // advertisement packets from them before connecting.
-            for (const device of devices) {
-                connectToDevice(device);
-            }
+          const service = await server?.getPrimaryService(serviceUuid);
+          characteristic = await service?.getCharacteristic(characteristicUuid);
+          document.getElementById("status").textContent = "Connected!";
         } catch (error) {
-            log('Argh! ' + error);
+          log("Argh! " + error);
         }
-    }, []);
+      },
+      { once: true },
+    );
 
-    const connectToDevice = useCallback(async (device: BluetoothDevice) => {
-        const abortController = new AbortController();
+    try {
+      log('Watching advertisements from "' + device.name + '"...');
+      await device.watchAdvertisements({ signal: abortController.signal });
+    } catch (error) {
+      log("Argh! " + error);
+    }
+  }, []);
 
-        device.addEventListener('advertisementreceived', async (event: BluetoothAdvertisingEvent) => {
-            log('> Received advertisement from "' + device.name + '"...');
-            // Stop watching advertisements to conserve battery life.
-            abortController.abort();
-            log('Connecting to GATT Server from "' + device.name + '"...');
-            try {
-                const server = await device.gatt?.connect()
-                log('> Bluetooth device "' + device.name + ' connected.');
+  const manualConnect = useCallback(async () => {
+    try {
+      log("Requesting any Bluetooth device...");
+      const device = await navigator.bluetooth.requestDevice({
+        filters: [
+          {
+            services: [serviceUuid],
+          },
+        ],
+      });
 
-                const service = await server?.getPrimaryService(serviceUuid);
-                characteristic = await service?.getCharacteristic(characteristicUuid);
-                document.getElementById('status').textContent = 'Connected!';
-            } catch (error) {
-                log('Argh! ' + error);
-            }
-        }, {once: true});
+      log("> Requested " + device.name);
+    } catch (error) {
+      log("Argh! " + error);
+    }
+  }, []);
 
-        try {
-            log('Watching advertisements from "' + device.name + '"...');
-            await device.watchAdvertisements({signal: abortController.signal});
-        } catch (error) {
-            log('Argh! ' + error);
-        }
-    }, [])
+  const startedRef = useRef(false);
 
-    const manualConnect = useCallback(async () => {
-        try {
-            log('Requesting any Bluetooth device...');
-            const device = await navigator.bluetooth.requestDevice({
-                filters: [{
-                    services: [serviceUuid]
-                }]
-            });
+  useEffect(() => {
+    if (!startedRef.current) {
+      autoConnect();
+    }
+    startedRef.current = true;
+  }, [autoConnect]);
 
-            log('> Requested ' + device.name);
-        } catch (error) {
-            log('Argh! ' + error);
-        }
-    }, []);
+  const sendCommand = useCallback(async (command: string) => {
+    if (characteristic) {
+      const encoder = new TextEncoder();
+      await characteristic.writeValue(encoder.encode(command));
+    }
+  }, []);
 
-    const startedRef = useRef(false);
+  const setColor = useCallback(async (color: string) => {
+    if (characteristic) {
+      const command = "COLOR:" + color.substring(1);
+      const encoder = new TextEncoder();
+      await characteristic.writeValue(encoder.encode(command));
+    }
+  }, []);
 
-    useEffect(() => {
-        if (!startedRef.current) {
-            autoConnect()
-        }
-        startedRef.current = true
-    }, [])
+  const setStripColor = useCallback(async (strip: number, color: string) => {
+    const command = "SCOLOR:" + strip + ":" + color.substring(1);
 
-    const sendCommand = useCallback(async (command: string) => {
-        if (characteristic) {
-            const encoder = new TextEncoder();
-            await characteristic.writeValue(encoder.encode(command));
-        }
-    }, []);
+    if (characteristic) {
+      const encoder = new TextEncoder();
+      await characteristic.writeValue(encoder.encode(command));
+    }
+  }, []);
 
-    const setColor = useCallback(async (color: string) => {
-        if (characteristic) {
-            const command = 'COLOR:' + color.substring(1);
-            const encoder = new TextEncoder();
-            await characteristic.writeValue(encoder.encode(command));
-        }
-    }, []);
+  return (
+    <div>
+      <div id="status">Not Connected</div>
+      <button className="button" onClick={manualConnect}>
+        Connect
+      </button>
+      <br />
+      <br />
+      <section>
+        <h1>Все вместе</h1>
+        <HexColorPicker onChange={(e) => setColor(e)} />
+      </section>
 
-    const setStripColor = useCallback(async (strip: number, color: string) => {
-        const command = 'SCOLOR:' + strip + ':' + color.substring(1);
+      <br />
+      <br />
 
-        if (characteristic) {
-            const encoder = new TextEncoder();
-            await characteristic.writeValue(encoder.encode(command));
-        }
-    }, [])
+      {MODES.map((mode) => {
+        return (
+          <button
+            key={mode}
+            className="button"
+            onClick={() => sendCommand(`MODE:${mode}`)}
+          >
+            {mode}
+          </button>
+        );
+      })}
 
-    const MODES = [
-        "SIMPLE", "EYES", "RANDOM"
-    ];
-
-    const ZONES = [{
-        id: 0,
-        name: 'висок верх'
-    },{
-        id: 1,
-        name: 'брови'
-    },{
-        id: 2,
-        name: 'лоб'
-    },{
-        id: 3,
-        name: 'левый глаз'
-    },{
-        id: 4,
-        name: 'подбородок'
-    },{
-        id: 5,
-        name: 'клюв'
-    },{
-        id: 6,
-        name: 'щека'
-    },{
-        id: 7,
-        name: 'правый глаз'
-    },{
-        id: 8,
-        name: 'висок'
-    }]
-
-    return <div>
-        <h1>BLE LED Control</h1>
-        <div id="status">Not Connected</div>
-        <button className="button" onClick={manualConnect}>Connect</button>
-        <br/><br/>
-        <input type="color" id="colorPicker" onChange={e => setColor(e.target.value)}/>
-        <br/><br/>
-
-        {MODES.map(mode => {
-            return <button key={mode} className="button" onClick={() => sendCommand(`MODE:${mode}`)}>{mode}</button>
-        })}
-
-        {ZONES.map(zone => {
-            return <div key={zone.id}>
-                {zone.name}
-                <input type="color" id={"colorPicker" + zone.id} onChange={e => setStripColor(zone.id, e.target.value)}/>
-                <br/><br/>
-            </div>
-        })}
+      {ZONES.map((zone) => {
+        return (
+          <section key={zone.id}>
+            <h1>{zone.name}</h1>
+            <HexColorPicker onChange={(e) => setStripColor(zone.id, e)} />
+          </section>
+        );
+      })}
     </div>
+  );
 }
 
-export default App
+export default App;
